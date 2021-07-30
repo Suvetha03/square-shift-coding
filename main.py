32503f14dc75b3e83619fc1f30ba3397d2013677
Suvetha03 <70473025+Suvetha03@users.noreply.github.com>
2021-07-31 00:56:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ for x in seating_arrangement:
         seating_arrangement_list.append(int(x))
     except:
         continue
-passenger_id = re.split(",", passenger_id[1:-1])
+passenger_id = re.split(',', passenger_id[1:-1])
 passenger_id = [int(x) for x in passenger_id]
 seating_arrangement = seating_arrangement_list
 
@@ -28,9 +28,6 @@ for i in range(len(seating_arrangement)):
         seating_arrangement_list.append(matrix)
         matrix = []
 
-print(seating_arrangement_list)
-
-
 # Arranging the passenger IDs according to the priority
 passenger_id = orderSeating(passenger_id)
 
@@ -39,9 +36,10 @@ aeroplane1 = AeroplaneSeating(seating_arrangement_list, passenger_id)
 aeroplane1.createSeating(seating_arrangement_list)
 index = aeroplane1.fillAisleSeats(passenger_id)
 passenger_id = passenger_id[index:]
-if index < len(passenger_id):
+if len(passenger_id) > 0:
     index = aeroplane1.fillWindowSeats(passenger_id)
 passenger_id = passenger_id[index:]
-if index < len(passenger_id):
+
+if len(passenger_id) > 0:
     aeroplane1.fillMiddleSeats(passenger_id)
 aeroplane1.displaySeating()
